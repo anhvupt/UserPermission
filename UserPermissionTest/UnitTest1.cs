cc732c2e3767486f2e16b59079eeee33a8e2dcec
anhvupt <anhvupt2412@gmail.com>
2020-06-01 20:48:18 +0700

    add test project

---
+++ UserPermissionTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UserPermissionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
